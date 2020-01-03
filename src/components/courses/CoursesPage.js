4077fdf9b476773d6ef9d107faab9dfa534c628b
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-01-03 11:37:41 +0000

    full redux flow for createCourse -and display them

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -37,6 +37,9 @@ class CoursesPage extends React.Component {
           value={this.state.course.title}
         />
         <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
       </form>
     );
   }
